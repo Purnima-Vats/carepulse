814cf2c30332cda35fa304c0b221131f00b31933
Purnima <purnimavats6789@gmail.com>
2024-07-06 21:20:22 +0530

    appwrite setup completed but taking too much time for creating user

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -11,6 +11,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
     INPUT = "input",
@@ -43,12 +44,15 @@ const PatientForm = () => {
         setIsLoading(true);
 
         try {
-            // const userData = { name, email, phone };
+            const userData = { name, email, phone };
 
-            // const user = await createUser(userData); 
+            const user = await createUser(userData); 
+            
 
-            // if(user) router.push(`/patients/${user.$id}/register`);
-
+            if(user) {
+                console.log("user created successfully")
+                router.push(`/patients/${user.$id}/register`);
+            }
         } catch (error) {
             console.log(error)
         }
