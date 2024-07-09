e0daa72849817e7ab62c15456c4307bb13db8c16
Purnima <purnimavats6789@gmail.com>
2024-07-09 22:35:21 +0530

    success page created successfully

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -18,3 +18,17 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
         console.log(error)
     }
 }
+
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId,
+        )
+
+        return parseStringify(appointment);
+    } catch (error) {
+        console.log(error)
+    }
+}
