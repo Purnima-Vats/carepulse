c025c266290230d265b291243323f29645748078
Purnima <purnimavats6789@gmail.com>
2024-07-10 18:50:07 +0530

    admin dashboard completed

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -5,26 +5,26 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Form } from "@/components/ui/form";
-import { createUser } from "@/lib/actions/patient.actions";
-import { CreateAppointmentSchema, getAppointmentSchema } from "@/lib/validation";
+import { Doctors } from "@/constants";
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions";
+import { getAppointmentSchema } from "@/lib/validation";
+import { Appointment } from "@/types/appwrite.types";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
-import { FormFieldType } from "./PatientForm";
-import { Doctors } from "@/constants";
 import { SelectItem } from "../ui/select";
-import Image from "next/image";
-import { createAppointment } from "@/lib/actions/appointment.actions";
+import { FormFieldType } from "./PatientForm";
 
 const AppointmentForm = ({
-    userId,
-    patientId,
-    type,
+    userId, patientId, type, appointment, setOpen
 }: {
     userId: string;
     patientId: string;
     type: "create" | "cancel" | "schedule";
+    appointment?: Appointment;
+    setOpen: (open: boolean) => void
 }) => {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
@@ -34,11 +34,11 @@ const AppointmentForm = ({
     const form = useForm<z.infer<typeof AppointmentFormValidation>>({
         resolver: zodResolver(AppointmentFormValidation),
         defaultValues: {
-            primaryPhysician: "",
-            schedule: new Date(),
-            reason: "",
-            note: "",
-            cancellationReason: "",
+            primaryPhysician: appointment ? appointment.primaryPhysician : "",
+            schedule: appointment ? new Date(appointment?.schedule) : new Date(Date.now()),
+            reason: appointment ? appointment.reason : "",
+            note: appointment?.note || "",
+            cancellationReason: appointment?.cancellationReason || "" ,
         },
     });
 
@@ -77,6 +77,25 @@ const AppointmentForm = ({
                     form.reset();
                     router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`);
                 }
+            } else{
+                const appointmentToUpdate = {
+                    userId,
+                    appointmentId: appointment?.$id!,
+                    appointment: {
+                        primaryPhysician: values.primaryPhysician,
+                        schedule: new Date(values?.schedule),
+                        status: status as Status,
+                        cancellationReason: values.cancellationReason,
+                    },
+                    type
+                }
+
+                const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+                if(updatedAppointment) {
+                    setOpen && setOpen(false);
+                    form.reset();
+                }
             }
 
         }catch (error) {
@@ -107,12 +126,12 @@ const AppointmentForm = ({
                 onSubmit={form.handleSubmit(onSubmit)}
                 className="space-y-6 flex-1"
             >
-                <section className="mb-12 space-y-4">
+                { type === "create" && <section className="mb-12 space-y-4">
                     <h1 className="header">New Appointment</h1>
                     <p className="text-dark-700">
                         Request a new appointment in 10 seconds
                     </p>
-                </section>
+                </section>}
 
                 {type !== "cancel" && (
                     <>
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 "use server"
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases} from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -28,6 +30,71 @@ export const getAppointment = async (appointmentId: string) => {
         )
 
         return parseStringify(appointment);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getRecentAppointmentList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [
+                Query.orderDesc("$createdAt")
+            ]
+        )
+
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+            if (appointment.status === "scheduled") {
+                acc.scheduledCount += 1;
+            } else if (appointment.status === "pending") {
+                acc.pendingCount += 1;
+            } else if (appointment.status === "cancelled") {
+                acc.cancelledCount += 1;
+            }
+
+            return acc;
+            
+        }, initialCounts);
+
+        const data =  {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents
+        }
+
+        return parseStringify(data);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateAppointment = async ({
+    appointmentId, userId, appointment, type
+}: UpdateAppointmentParams) => {
+    try {
+        const updatedAppointment = await databases.updateDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId,
+            appointment
+        )
+
+        if(!updatedAppointment){
+            throw new Error("Could not update appointment")
+        }
+
+        // TODO: SMS notification
+
+        revalidatePath("/admin");
+        return parseStringify(updatedAppointment);
     } catch (error) {
         console.log(error)
     }
